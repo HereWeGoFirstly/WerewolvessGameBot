be8a406defb94463dac96ced989e6f50185ce4f4
HereWeGoFirstly <Chimichangi1111>
2023-11-03 21:15:03 +0300

    init

---
+++ src/main/java/ru/telegram/games/werewolvessgamebot/WerewolvessGameBotApplication.java
@@ -0,0 +1,13 @@
+package ru.telegram.games.werewolvessgamebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerewolvessGameBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WerewolvessGameBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/telegram/games/werewolvessgamebot/WerewolvessGameBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.telegram.games.werewolvessgamebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerewolvessGameBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
